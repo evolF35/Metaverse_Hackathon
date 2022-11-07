de3e01fe8234740351678b13c5628b60016e2f4c
evolF35 <maccyair@gmail.com>
2022-11-07 05:22:04 -0800

    gosh i wish they updated this more often

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,19 @@
 require("@nomicfoundation/hardhat-toolbox");
+require('dotenv').config()
+
+const optimismGoerliUrl =
+   process.env.ALCHEMY_API_KEY ?
+      `https://opt-goerli.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}` :
+      process.env.OPTIMISM_GOERLI_URL
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.9",
 };
+
+module.exports.networks = {
+  "optimism-goerli": {
+    url: optimismGoerliUrl,
+    accounts: { mnemonic: process.env.MNEMONIC }
+ }   
+}
